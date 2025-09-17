0bce030bdaea5b80ba6f47e97d4a0e5f87dd6514
tokiwa-app <admin@tokiwa-logistics.com>
2025-09-17 18:57:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Akiyama Order!");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
